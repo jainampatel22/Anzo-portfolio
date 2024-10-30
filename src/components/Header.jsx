2b5af96a2e91cc0140996a7e33ade3a90e463853
Jainam <pateljainams122@gmail.com>
2024-10-30 11:12:56 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import 'remixicon/fonts/remixicon.css'
 const Header = () => {
   return (
 <div  className='fixed w-full z-10 p-16 flex items-center justify-end'>
-    <button className=' border-4 text-xl  bg-black px-9 py-2 rounded-full text-white  hover:bg-gray-500'>Hire me</button>
+   <a href="https://x.com/Jainam___patel?t=JHsFJ-r9qL9gyANyefgew&s=09"><button className=' border-4 text-xl  bg-black px-9 py-2 rounded-full text-white  hover:bg-gray-500'>Hire me</button></a> 
     <i className="ri-more-2-fill text-2xl ml-1"></i>
 </div>
   )
